6b4e3b7b873a0476a03611b75c6a67c0e08bdb7e
SwetaDhanvantri <shikhadhanvantri@gmail.com>
2025-01-11 17:26:41 +0530

    pin app

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("darkbg");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".header").removeClass("darkbg");
+    }
+});
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
